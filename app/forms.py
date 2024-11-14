40abf8cf3c25df11fade8004f1bbc1eeb12272e3
jysndabuio <jysndabu@outlook.com>
2024-11-14 21:32:14 +0100

    Revise Registration Page

--- app/forms.py
+++ app/forms.py
@@ -1,28 +1,24 @@
 # forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, TelField, SubmitField, HiddenField, EmailField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
+from wtforms import StringField, PasswordField, SelectField, TelField, SubmitField, HiddenField, EmailField, IntegerField, RadioField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp, NumberRange
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    role = HiddenField(default='patient')  # Set role as hidden with default "patient"
+    role = HiddenField(default='doctor')  # Set role as hidden with default "patient"
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
-    phone = TelField('Phone Number', validators=[DataRequired()])
-    street = StringField('House Number, Street', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    postal = StringField('Postal Code', validators=[DataRequired()])
-    control_number = StringField('ID number (for Doctors only)')
-    security_question = SelectField('Security Question', choices=[
-        ('pet_name', 'What is the name of your first pet?'),
-        ('mother_maiden', 'What is your mother\'s maiden name?'),
-        ('favorite_color', 'What is your favorite color?'),
-        ('city_born', 'What city were you born in?'),
-        ('high_school', 'What high school did you attend?')
+    age = IntegerField('Age', validators=[DataRequired()])
+    gender = RadioField('M/W/F', choices=[
+            ('male', 'Male'), # 'male' is the value, 'Male' is the label
+            ('female', 'Female'),
+            ('other', 'Other')
     ], validators=[DataRequired()])
-    security_answer = StringField('Answer', validators=[DataRequired()])
-    
+    contact_number=IntegerField(validators=[DataRequired()])
+    id_card_number = IntegerField('ID number', validators=[DataRequired(), NumberRange(min=11, max=11, message='Please provide your 11 digit identification card number')])
+    license_number=IntegerField('License Number', validators=[DataRequired()])
+    home_address=StringField('Complete Home Adress', validators=[DataRequired()]) 
     password = PasswordField('Password', validators=[
         DataRequired(),
         Length(min=8, message="Password must be at least 8 characters long"),
