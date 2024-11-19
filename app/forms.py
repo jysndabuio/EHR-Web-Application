a67acc88bfacf4b148efa9cbc81fc633dd205215
jysndabuio <jysndabu@outlook.com>
2024-11-19 15:15:44 +0100

    Working: updating doctors info

--- app/forms.py
+++ app/forms.py
@@ -53,10 +53,10 @@ class UserUpdateProfile(FlaskForm):
     ecd_contact_number = StringField('Emergency Contact Number', validators=[Optional()])
 
     current_password = PasswordField('Current Password', validators=[
-        Optional(),  # Current password is optional unless changing the password
+        Optional()
     ])
     password = PasswordField('New Password', validators=[
-        Optional(),  # New password is optional for profile updates
+        Optional(),
         Length(min=8, message="Password must be at least 8 characters long"),
         Regexp(r'^(?=.*[A-Z])(?=.*\d)', message="Password must contain at least one uppercase letter and one number.")
     ])
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, date
-from . import db  # Import db after it's initialized in __init__.py
+from . import db, bcrypt  # Import db after it's initialized in __init__.py
 from flask_login import UserMixin
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
@@ -35,6 +35,14 @@ class User(UserMixin, db.Model):
     
     def get_id(self):
         return self.id
+    
+    def set_password(self, password):
+        """Hashes and sets the user's password."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        """Checks if the provided password matches the stored hash."""
+        return bcrypt.check_password_hash(self.password, password)
 
 class UserEducation(UserMixin, db.Model):
     __tablename__ = 'user_education'
@@ -50,7 +58,7 @@ class UserEducation(UserMixin, db.Model):
     board_cert = db.Column(db.String(50), nullable=True)
     license_number = db.Column(db.String(50), nullable=False)
     license_issuer = db.Column(db.String(50), nullable=True)
-    license_expiration = db.Column(db.String(50), nullable=True)
+    license_expiration = db.Column(db.Date, nullable=True)
     years_of_experience = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
